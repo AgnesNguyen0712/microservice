baf9abb904054ffb00f01a423f1cf9c03f7929c8
Thao Nguyen <ntthuthao0712@gmail.com>
2022-02-17 23:20:08 +0700

    commit

--- notification-server/src/main/java/com/example/notificationserver/Listener.java
+++ notification-server/src/main/java/com/example/notificationserver/Listener.java
@@ -1,9 +1,6 @@
 package com.example.notificationserver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
@@ -11,18 +8,8 @@ import javax.jms.JMSException;
 @Component
 public class Listener {
 
-    @Autowired
-    private JavaMailSender emailSender;
-
     @JmsListener(destination = "email-event")
     public void receiveMessage(final String jsonMessage) throws JMSException {
         System.out.println("Received message " + jsonMessage);
-        SimpleMailMessage messageSend = new SimpleMailMessage();
-        messageSend.setFrom("ntthuthao0712@gmail.com");
-        messageSend.setTo("nthithuthao@tma.com.vn");
-        messageSend.setSubject("Order Information");
-        messageSend.setText("Sended order "+ "1");
-        emailSender.send(messageSend);
-
     }
 }
