fa302b424e3e2eaa3fbb99834dd39e72bc446685
Thao Nguyen <ntthuthao0712@gmail.com>
2022-02-18 13:50:19 +0700

    add send email

--- notification-server/src/main/java/com/example/notificationserver/Listener.java
+++ notification-server/src/main/java/com/example/notificationserver/Listener.java
@@ -1,15 +1,26 @@
 package com.example.notificationserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
 
 @Component
 public class Listener {
-
+	  @Autowired
+	    public JavaMailSender emailSender;
     @JmsListener(destination = "email-event")
     public void receiveMessage(final String jsonMessage) throws JMSException {
         System.out.println("Received message " + jsonMessage);
+        // Create a Simple MailMessage.
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("ntthuthao0712@gmail.com");
+        message.setSubject("Order infor");
+        message.setText(jsonMessage);
+        // Send Message!
+        this.emailSender.send(message);
     }
 }
--- notification-server/src/main/java/com/example/notificationserver/NotificationServerApplication.java
+++ notification-server/src/main/java/com/example/notificationserver/NotificationServerApplication.java
@@ -1,9 +1,15 @@
 package com.example.notificationserver;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,3 +19,25 @@ public class NotificationServerApplication {
 		SpringApplication.run(NotificationServerApplication.class, args);
 	}
 }
+
+@Configuration
+class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("thaob1605307@student.ctu.edu.vn");
+        mailSender.setPassword("3dUBy3vn");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+    }
